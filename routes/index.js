a993fc9b3599f60ec8e67aed4ef42fe1a18fa36d
sin01.hu <sin01.hu@vipshop.com>
2017-01-15 23:28:25 +0800

    The article reads github api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const router = require('koa-router')()
+const request = require('co-request')
+
+//https://api.github.com/repos/hoosin/hoosin.github.io/issues
+
 
 router.get('/', async(ctx, next) => {
+
+  let result = await request({
+    url: 'https://api.github.com/repos/hoosin/hoosin.github.io/issues',
+    headers: {
+      'User-Agent': 'request'
+    }
+  })
+
+  let body = result.body
+
   ctx.state = {
-    title: 'hoosin (@hoosin) blogs'
+    title: 'hoosin (@hoosin) blogs',
+    body: JSON.parse(body)
   }
 
   await ctx.render('index', {})
